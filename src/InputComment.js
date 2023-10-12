00d6d61f31315c6a70a98981e582eeec32d347a7
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-12 23:40:23 +0530

    Setting input-comment state

--- src/InputComment.js
+++ src/InputComment.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import logo1 from './images/avatars/image-juliusomo.png';
 
 function InputComment() {
+
+  const [comment, setComment] = useState('');
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  }
+
   return (
     <div className="input-comment-section">
       <img className='profile-image' src={logo1} alt="" />
-      <textarea className='input-box-comment' type="text" />
+      <textarea onChange={handleChange} className='input-box-comment' type="text" />
       <button className='button'>Send</button>
     </div>
   )
