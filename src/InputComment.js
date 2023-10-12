824de90a38837249c39a6625b4d181af1e949a8d
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-12 23:37:54 +0530

    Styled input-comment

--- src/InputComment.js
+++ src/InputComment.js
@@ -1,10 +1,13 @@
+import logo1 from './images/avatars/image-juliusomo.png';
 
 function InputComment() {
   return (
     <div className="input-comment-section">
-      Comment section
+      <img className='profile-image' src={logo1} alt="" />
+      <textarea className='input-box-comment' type="text" />
+      <button className='button'>Send</button>
     </div>
   )
 }
 
-export default InputComment
+export default InputComment;
