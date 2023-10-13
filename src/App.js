0522cd6ffd47ed47104c202f6426484ce5c51ae1
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-13 23:31:03 +0530

    Comments array mapping

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import CommentInfo from "./CommentInfo";
 import InputComment from "./InputComment";
 
 function App() {
+
+  const [comments, setComments] = useState([]);
+
+  const handleClick = (event) => {
+    setComments([...comments, event]);
+  }
+
   return (
     <div className="main-section">
       <div className="main-top">
-        <CommentInfo />
-        <CommentInfo />
-        <CommentInfo />
-        <CommentInfo />
-        <CommentInfo />
+        {comments.map(() => (<CommentInfo />))}
       </div>
       <div className="main-bottom">
-        <InputComment />
+        <InputComment onChange={handleClick} />
       </div>
     </div>
   );
--- src/InputComment.js
+++ src/InputComment.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import logo1 from './images/avatars/image-juliusomo.png';
 
-function InputComment() {
+function InputComment({onChange}) {
 
   const [comment, setComment] = useState('');
 
@@ -9,11 +9,16 @@ function InputComment() {
     setComment(e.target.value);
   }
 
+  const btnClick = () => {
+    onChange(comment);
+    setComment('');
+  }
+
   return (
     <div className="input-comment-section">
       <img className='profile-image' src={logo1} alt="" />
-      <textarea onChange={handleChange} className='input-box-comment' type="text" />
-      <button className='button'>Send</button>
+      <textarea value={comment} onChange={handleChange} className='input-box-comment' type="text" />
+      <button onClick={btnClick} className='button'>Send</button>
     </div>
   )
 }
